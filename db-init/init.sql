8f2dbc34f30185971a8fb9fe6f5b91026a6c2ba0
decoNR <andrenogueiraribeiro@usp.br>
2023-06-22 16:17:14 -0300

    add de inserçao no init.sql

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,4 +1,4 @@
-/*CREATE TABLE pessoa (
+CREATE TABLE pessoa (
     id serial,
     nusp character varying(10) NOT NULL,
     nome character varying(255) NOT NULL,
@@ -37,80 +37,86 @@ CREATE FUNCTION lista_pessoaperfil()
 RETURNS SETOF pessoaperfil AS $$
     SELECT * FROM pessoaperfil
     ORDER BY data_inicio;
-$$ LANGUAGE SQL;*/
+$$ LANGUAGE SQL;
 
-CREATE TABLE pessoa (
-  id SERIAL PRIMARY KEY,
-  cpf VARCHAR(11) UNIQUE,
-  name TEXT,
-  endereco TEXT,
-  instituicao TEXT,
-  login TEXT,
-  senha TEXT,
-  data_nasc TIMESTAMP
-);
+-- CREATE TABLE pessoa (
+--   id SERIAL PRIMARY KEY,
+--   cpf VARCHAR(11) UNIQUE,
+--   name TEXT,
+--   endereco TEXT,
+--   instituicao TEXT,
+--   login TEXT,
+--   senha TEXT,
+--   data_nasc TIMESTAMP
+-- );
 
-CREATE TABLE aluno (
-  id SERIAL PRIMARY KEY,
-  id_pessoa SERIAL,
-  nota_ingresso REAL,
-  curso TEXT,
-  FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
-);
+-- INSERT INTO pessoa (cpf, name, endereco, instituicao, login, senha, data_nasc)
+-- VALUES ('12345678901', 'João Silva', 'Rua Principal, 123', 'Universidade XYZ', 'joaosilva', 'senhasecreta', '2000-01-01 00:00:00');
 
-CREATE TABLE funcionario (
-  id SERIAL PRIMARY KEY,
-  id_pessoa SERIAL,
-  funcao TEXT,
-  FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
-);
+-- INSERT INTO pessoa (cpf, name, endereco, instituicao, login, senha, data_nasc)
+-- VALUES ('2134564901', 'Bruno Ferber', 'Rua Secundaria, 321', 'Universidade ABC', 'bruno', 'senhasecreta', '2003-11-01 04:04:40');
 
-CREATE TABLE docente (
-  id SERIAL PRIMARY KEY,
-  id_pessoa SERIAL,
-  especialidade TEXT,
-  FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
-);
+-- CREATE TABLE aluno (
+--   id SERIAL PRIMARY KEY,
+--   id_pessoa SERIAL,
+--   nota_ingresso REAL,
+--   curso TEXT,
+--   FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
+-- );
 
-CREATE TABLE perfil (
-  id SERIAL PRIMARY KEY,
-  codigo TEXT UNIQUE,
-  tipo TEXT
-);
+-- CREATE TABLE funcionario (
+--   id SERIAL PRIMARY KEY,
+--   id_pessoa SERIAL,
+--   funcao TEXT,
+--   FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
+-- );
 
-CREATE TABLE servico (
-    id SERIAL PRIMARY KEY,
-    id_perfil SERIAL,
-    codigo TEXT UNIQUE,
-    tipo TEXT,
-    descricao TEXT,
-    FOREIGN KEY (id_perfil) REFERENCES perfil (id)
-);
+-- CREATE TABLE docente (
+--   id SERIAL PRIMARY KEY,
+--   id_pessoa SERIAL,
+--   especialidade TEXT,
+--   FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
+-- );
 
-CREATE TABLE perfil_pessoa (
-    id SERIAL PRIMARY KEY,
-    id_pessoa SERIAL,
-    id_perfil SERIAL,
-    FOREIGN KEY (id_pessoa) REFERENCES pessoa (id),
-    FOREIGN KEY (id_perfil) REFERENCES perfil (id)
-);
+-- CREATE TABLE perfil (
+--   id SERIAL PRIMARY KEY,
+--   codigo TEXT UNIQUE,
+--   tipo TEXT
+-- );
 
-CREATE TABLE disciplina (
-    id SERIAL PRIMARY KEY,
-    codigo TEXT UNIQUE,
-    ementa TEXT,
-    data_criacao TIMESTAMP
-);
+-- CREATE TABLE servico (
+--     id SERIAL PRIMARY KEY,
+--     id_perfil SERIAL,
+--     codigo TEXT UNIQUE,
+--     tipo TEXT,
+--     descricao TEXT,
+--     FOREIGN KEY (id_perfil) REFERENCES perfil (id)
+-- );
 
-CREATE TABLE matricula (
-    id SERIAL PRIMARY KEY,
-    id_aluno SERIAL,
-    id_docente SERIAL,
-    id_disciplina SERIAL,
-    data_ini TIMESTAMP,
-    data_fim TIMESTAMP,
-    nota REAL,
-    FOREIGN KEY (id_aluno) REFERENCES aluno (id),
-    FOREIGN KEY (id_docente) REFERENCES docente (id),
-    FOREIGN KEY (id_disciplina) REFERENCES disciplina (id)
-);
+-- CREATE TABLE perfil_pessoa (
+--     id SERIAL PRIMARY KEY,
+--     id_pessoa SERIAL,
+--     id_perfil SERIAL,
+--     FOREIGN KEY (id_pessoa) REFERENCES pessoa (id),
+--     FOREIGN KEY (id_perfil) REFERENCES perfil (id)
+-- );
+
+-- CREATE TABLE disciplina (
+--     id SERIAL PRIMARY KEY,
+--     codigo TEXT UNIQUE,
+--     ementa TEXT,
+--     data_criacao TIMESTAMP
+-- );
+
+-- CREATE TABLE matricula (
+--     id SERIAL PRIMARY KEY,
+--     id_aluno SERIAL,
+--     id_docente SERIAL,
+--     id_disciplina SERIAL,
+--     data_ini TIMESTAMP,
+--     data_fim TIMESTAMP,
+--     nota REAL,
+--     FOREIGN KEY (id_aluno) REFERENCES aluno (id),
+--     FOREIGN KEY (id_docente) REFERENCES docente (id),
+--     FOREIGN KEY (id_disciplina) REFERENCES disciplina (id)
+-- );
